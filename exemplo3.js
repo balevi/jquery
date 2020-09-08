72ad4a2f7af2e25f3dfde04723b918c6c7a14101
Levi Almeida <levialmeida1994@hotmail.com>
2020-09-08 14:15:48 -0300

    comando para trabalhar scroll

--- exemplo3.js
+++ exemplo3.js
@@ -1,7 +1,20 @@
 $(function(){
-
+    /*  verifica se imagem estiver com algum erro de endereço e coloca outra definida
     $('img').on("error",function(){
         $(this).attr("src","img/negativo.jpg");
 
+    });
+    */ 
+
+    // maneira de sumir imagem e aparece a depender da rolagem do scroll
+    $('body').css("height","5000px");
+    $(window).scroll(function(){
+        var topo = $(window).scrollTop();
+        if(topo>50){
+            $('img').fadeOut('1000');
+        } else{
+            $('img').fadeIn('1000');
+        }
+    
     });
 });
